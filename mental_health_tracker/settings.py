12eb7d190918338d941fed0343d33871fc2c9758
Brian Altan <brianaltan28@gmail.com>
2024-09-16 16:33:31 +0700

    Tutorial 2: fix github actions

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -121,4 +121,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["localhost", "127.0.0.1", "http://brian-altan-mentalhealthtracker.pbp.cs.ui.ac.id", "https://brian-altan-mentalhealthtracker.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["http://brian-altan-mentalhealthtracker.pbp.cs.ui.ac.id", "https://brian-altan-mentalhealthtracker.pbp.cs.ui.ac.id"]
